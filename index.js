e2901e1cd5fed2d12419dcda63a611fec0f6ac60
Christine Patton <christine@pattoncreative.com>
2018-07-19 04:46:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(somevalue) {
+  return Math.abs(somevalue - 42)
+}
+
+function distanceFromHqInFeet(somevalue) {
+  return Math.abs((somevalue - 42)*264)
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs((start-destination))*264
+}
+
+
+function calculatesFarePrice (start, destination) {
+  if ((Math.abs(start-destination)*264)<=400) {
+   return 0;
+}
+   else if (((Math.abs(start-destination)*264)>400), ((Math.abs(start-destination)*264)<2000)) {
+   return ((Math.abs(start-destination)*264)-400)*0.02;
+}
+   else if (((Math.abs(start-destination)*264)>2000), ((Math.abs(start-destination)*264)<2500)) {
+   return 25;
+}
+   else if (((Math.abs(start-destination)*264)>2500)) {
+   return "cannot travel that far";
+}
+}
+ 
+ 
